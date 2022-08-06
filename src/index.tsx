9dbb49f99e0127d0b5fa6c6ac2d6996743e3e9ed
Takaaki-Shimizu <tattaka_feelmysoul@yahoo.co.jp>
2022-08-06 10:48:29 +0900

    Loginコンポーネント作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Route, BrowserRouter } from "react-router-dom";
+import Login from "./Login";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={Login} />
+      </>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
